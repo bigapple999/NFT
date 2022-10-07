9fd7332808987b27d319fbe34fe99145e4282eeb
bigapple999 <102247322+bigapple999@users.noreply.github.com>
2022-10-07 22:37:02 +0800

    Update IERC721Metadata.sol

--- ERC721A/Contract/IERC721Metadata.sol
+++ ERC721A/Contract/IERC721Metadata.sol
@@ -3,6 +3,9 @@
  * @title ERC-721 Non-Fungible Token Standard, optional metadata extension
  * @dev See https://eips.ethereum.org/EIPS/eip-721
  */
+
+import "./IERC721.sol";
+
 interface IERC721Metadata is IERC721 {
     /**
      * @dev Returns the token collection name.
