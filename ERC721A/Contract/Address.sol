685c0205a2d08eee57ea46956606b0625eb8e088
bigapple999 <102247322+bigapple999@users.noreply.github.com>
2022-10-07 22:45:46 +0800

    Update Address.sol

--- ERC721A/Contract/Address.sol
+++ ERC721A/Contract/Address.sol
@@ -1,8 +1,8 @@
-
 // File: @openzeppelin/contracts/utils/Address.sol
 /**
  * @dev Collection of functions related to the address type
  */
+ 
 library Address {
     /**
      * @dev Returns true if `account` is a contract.
